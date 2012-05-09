c3497064afe86980301e6dfdac0c2b113a9f73e4
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2012-05-09 14:01:20 +0100

    Fix launcher typo and add check for JQuery.

--- tools/wizzywig/src/main/javascript/launcher.js
+++ tools/wizzywig/src/main/javascript/launcher.js
@@ -1,6 +1,11 @@
 //
 
+if (!$) alert('' +
+        'You must include JQuery to run WizzyWig. Consider adding this to the top of your HTML:\n' +
+        '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>'
+);
+
 $(document).ready(function() {
     new CssLoader(cssUrl());
-    new WizzyWig(new Saver(), WizzyWig,editableElements);
+    new WizzyWig(new Saver(), WizzyWig.editableElements);
 });
